c102f5efa8464e8426db3794005de6abd1c21f0a
Dev Jana <devjana@Devs-MacBook-Pro.local>
2016-05-17 21:42:32 -0500

    user input added

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -8,3 +8,12 @@ var longerString = function( string1, string2 ){
     return string2;
   }
 }
+
+var userInput = function(){
+  console.log( "in userInput" );
+  var input1 = document.getElementById('input1').value;
+  console.log( "input1: "+  input1 );
+  var input2 = document.getElementById('input2').value;
+  console.log( "input2: "+  input2 );
+  alert( "Longer String: " + longerString( input1, input2 ) );
+}
