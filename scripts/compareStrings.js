1c07d6eb07b6499a509682e8e2428bb4534f6a80
Dev Jana <devjana@Devs-MacBook-Pro.local>
2016-05-17 22:03:07 -0500

    white space handling

--- scripts/compareStrings.js
+++ scripts/compareStrings.js
@@ -1,3 +1,9 @@
+var hasWhiteSpace = function( word ) {
+  console.log( "in hasWhiteSpace, checking " + word );
+  // returns true if has whitespace
+  return word.indexOf(' ') >= 0;
+}
+
 var longerString = function( string1, string2 ){
   console.log( "in longerString, Comparing: " + string1 + " & " + string2 );
   // return longer string
@@ -15,5 +21,11 @@ var userInput = function(){
   console.log( "input1: "+  input1 );
   var input2 = document.getElementById('input2').value;
   console.log( "input2: "+  input2 );
-  alert( "Longer String: " + longerString( input1, input2 ) );
+  
+  if( hasWhiteSpace( input1 ) || hasWhiteSpace( input2 ) ){
+    alert( "no whitespaces!!!" );
+  }
+  else{
+    alert( "Longer String: " + longerString( input1, input2 ) );
+  }
 }
